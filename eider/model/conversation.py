eeb61f95c1a52073a6b747935b7af0d2c6a22d3f
Amin Etesamian <aminetesamian1371@gmail.com>
2018-04-29 17:21:12 +0430

    Message model

--- eider/model/conversation.py
+++ eider/model/conversation.py
@@ -9,5 +9,13 @@ class Conversation(db.Model):
     __tablename__ = 'conversations'
 
     uid = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.Unicode(), nullable=True)
+
+
+class Message(db.Model):
+    __tablename__ = 'messages'
+
+    uid = db.Column(db.Integer(), primary_key=True)
+    conversation_uid = db.Column(db.Integer, db.ForeignKey('conversations.uid'))
+    author_uid = db.Column(db.Integer, db.ForiegnKey('users.uid'))
+    text = db.Column(db.Unicode(), nullable=True)
     created = db.Column(db.DateTime(), default=datetime.now)
